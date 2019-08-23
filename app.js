27dee1ca67520b7745b9609ca4dcc3d60677ce92
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-08-23 12:29:54 -0700

    merge error fix

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert('dude js working!');
 
-<<<<<<< HEAD
+
 let btnDo = document.getElementById('btnDo');
 
 btnDo.addEventListener('click', ()=>{alert('yolo')})
-=======
+
+
 let btnDO =document.getElementById('btnDO');
 
 btnDO.addEventListener ('click', ()=>{alert('yolo')});
->>>>>>> master
+
