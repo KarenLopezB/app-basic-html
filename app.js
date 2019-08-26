e373799d19d0f736e52dc6aaae6eab9e9c966feb
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-08-26 09:57:07 -0700

    app.js modificacion

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-alert('dude js working!');
+const buttons = document.querySelectorAll('button'); 
+
+const playnote = event =>{
+    console.log(event);
+    alert('hola');
+}
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote)
+);
 
 
-let btnDo = document.getElementById('btnDo');
-
-btnDo.addEventListener('click', ()=>{alert('yolo')})
-
-
-let btnDO =document.getElementById('btnDO');
-
-btnDO.addEventListener ('click', ()=>{alert('yolo')});
+//alert('dude js working!');
+//let btnDo = document.getElementById('btnDo');
+//btnDo.addEventListener('click', ()=>{alert('yolo')})
+//let btnDO =document.getElementById('btnDO');
+//btnDO.addEventListener ('click', ()=>{alert('yolo')});
 
