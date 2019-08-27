a51dc111d44f7e5e774dc5ed79ac8c3fb9bf278a
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-08-27 11:58:15 -0700

    notas musicales con sonido

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const buttons = document.querySelectorAll('button'); 
+const buttons = document.querySelectorAll('button');
 
-const playnote = event =>{
-    console.log(event);
-    alert('hola');
+let playNote = event => {
+    console.log(event.target)
+    const button = event.target;
+    const note = button.dataset.note;
+    console.log(`audio${note}`);
+    const audio = document.getElementById(`audio${note}`);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 }
 
 buttons.forEach(
-    button => button.addEventListener('click', playnote)
+    button => button.addEventListener('click', playNote)
 );
 
-
-//alert('dude js working!');
-//let btnDo = document.getElementById('btnDo');
-//btnDo.addEventListener('click', ()=>{alert('yolo')})
-//let btnDO =document.getElementById('btnDO');
-//btnDO.addEventListener ('click', ()=>{alert('yolo')});
-
+document.addEventListener('keydown', 
+    event => {
+        const key = event.key;
+        const button = document.querySelector(`button[data-key="${key}"]`);
+        if(button) button.click();
+    }
+);
