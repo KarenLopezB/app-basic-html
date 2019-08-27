58b74119d8c71729cab24a1189e44f84006ad736
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-08-27 12:59:39 -0700

    notas musicales playnote

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let playNote = event => {
     console.log(event.target)
     const button = event.target;
     const note = button.dataset.note;
-    console.log(`audio${note}`);
+    console.log(`audio${note}`); //concatenar un texto con emascript 6 (atajo para poner las comillas invertidas : alt+96, pero en este teclado es : ctrl+alt+(icono)) 
     const audio = document.getElementById(`audio${note}`);
     audio.pause();
-    audio.currentTime = 0;
+    audio.currentTime = 0; 
     audio.play();
 }
 
